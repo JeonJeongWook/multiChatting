7c1171b1ac297361397bd14249d806e00a154494
JeonJeongWook <jjwook324@gmail.com>
2019-11-16 00:51:08 +0900

    gui 1차 완료

--- src/com/example/gui/ChattingRoom.java
+++ src/com/example/gui/ChattingRoom.java
@@ -2,9 +2,13 @@ package com.example.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,7 +19,8 @@ import javax.swing.JTextField;
 public class ChattingRoom extends JFrame{
 	public JPanel p_chatting = new JPanel();
 	public JPanel p_userList = new JPanel();
-	public JLabel lb_users[] = new JLabel[10];	//유저 10명
+	public JPanel chatBar = new JPanel();
+	public JLabel[] lb_users = new JLabel[10];
 	public JTextArea ta_chatlog = new JTextArea();
 	public JTextField tf_chatting = new JTextField(20);
 	public JButton btn_send = new JButton("보내기");
@@ -25,10 +30,28 @@ public class ChattingRoom extends JFrame{
 		p_userList.setBackground(Color.white);
 		
 		p_userList.setLayout(new GridLayout(10,1));
+		for(int i=0; i<lb_users.length; i++) {
+			lb_users[i] = new JLabel(""+i);
+			lb_users[i].setPreferredSize(new Dimension(100, 100));
+			lb_users[i].setBorder(BorderFactory.createLineBorder(Color.yellow,2));
+			p_userList.add(lb_users[i]);
+		}
+		chatBar.add(tf_chatting);
+		chatBar.add(btn_send);
+		
+		p_chatting.setLayout(new BorderLayout());
+		p_chatting.add(ta_chatlog, BorderLayout.CENTER);
+		p_chatting.add(chatBar, BorderLayout.SOUTH);
 		
 		add(p_userList,BorderLayout.EAST);
 		add(p_chatting,BorderLayout.CENTER);
 		
+		btn_send.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("보내기를 눌렀습니다");
+			}
+		});
 	}
 	
 	public void display() {
